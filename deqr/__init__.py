43b269315fe98edf3dc4922752467274d7c239b0
torque <torque@users.noreply.github.com>
2021-06-23 21:51:00 -0700

    package: use poetry-dynamic-versioning for versioning.
    This also lets us easily add a __version__ attribute to the package
    itself, which is good. Even though it seemed to work well, I always
    felt a bit nervous about the importlib approach. Well, we still use it
    for development installs, but packaged versions will have a hardcoded
    value.

--- deqr/__init__.py
+++ deqr/__init__.py
@@ -1,3 +1,13 @@
+__version__ = "__PLACEHOLDER__"
+
+# this is to handle local development installs, where the version replacement
+# gets reverted after the installation finishes.
+if __version__ == "__PLACEHOLDER__":
+    from importlib import metadata
+
+    __version__ = metadata.version(__name__)
+
+
 from . import image, datatypes
 from .datatypes import QREccLevel, QRDataType, QRCodeData, QRCode
 from .quirc import QuircDecoder
