5ea9a83e4e141bdd1b804cf00b8425441c7d725a
torque <torque@users.noreply.github.com>
2023-06-16 23:58:23 -0700

    build: remove poetry-dynamic-versioning
    It's convenient, but too flaky. Maintaining the version information in
    two places is not an unbearable maintenance burden, and CI can check
    for mismatches prior to publishing.

--- deqr/__init__.py
+++ deqr/__init__.py
@@ -1,12 +1,5 @@
-__version__ = "__PLACEHOLDER__"
-
-# this is to handle local development installs, where the version replacement
-# gets reverted after the installation finishes.
-if __version__ == "__PLACEHOLDER__":
-    from importlib import metadata
-
-    __version__ = metadata.version(__name__)
-
+__version__ = "0.2.2"
+__VERSION__ = __version__
 
 from . import image, datatypes
 from .datatypes import QREccLevel, QRDataType, QRCodeData, QRCode
